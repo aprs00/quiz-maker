b33d794ced134e11760e87c8fc56e66743901e4f
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 00:13:25 +0200

    removed unnecessary import

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-import {USE_MOCK_API} from './config/env.ts';
+// import {USE_MOCK_API} from './config/env.ts';
 import {worker} from './mocks/browser.ts';
 
-if (USE_MOCK_API) worker.start();
+worker.start();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
