c27b91fb23f1c90bf005d32c34a604f93381fd8d
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 11:03:01 +0200

    changed worker init

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,7 @@ import App from './App.tsx';
 import {USE_MOCK_API} from './config/env.ts';
 import {worker} from './mocks/browser.ts';
 
-if (USE_MOCK_API)
-    worker.start({
-        serviceWorker: {
-            url: 'https://quiz-maker-blue.vercel.app/mockServiceWorker.js',
-        },
-    });
+if (USE_MOCK_API) worker.start();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
