be8136fa558d29c7becfddccff34e3e26c874dcd
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 10:59:49 +0200

    added .env.example

--- src/main.tsx
+++ src/main.tsx
@@ -5,27 +5,15 @@ import App from './App.tsx';
 import {USE_MOCK_API} from './config/env.ts';
 import {worker} from './mocks/browser.ts';
 
-// worker.start();
+if (USE_MOCK_API)
+    worker.start({
+        serviceWorker: {
+            url: 'https://quiz-maker-blue.vercel.app/mockServiceWorker.js',
+        },
+    });
 
-async function main() {
-    if (USE_MOCK_API) {
-        if (window.location.pathname === '/quizzes') {
-            window.location.pathname = '/quizzes/';
-            return;
-        }
-
-        await worker.start({
-            serviceWorker: {
-                url: 'https://quiz-maker-blue.vercel.app/mockServiceWorker.js',
-            },
-        });
-    }
-
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>,
-    );
-}
-
-main();
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
