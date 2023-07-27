111c2bdfb8d5f2c56d6a0eb52c1a161805964be3
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 09:50:50 +0200

    added vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-// import {USE_MOCK_API} from './config/env.ts';
+import {USE_MOCK_API} from './config/env.ts';
 import {worker} from './mocks/browser.ts';
 
-worker.start();
+// worker.start();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+async function main() {
+    if (USE_MOCK_API) {
+        if (window.location.pathname === '/quizzes') {
+            window.location.pathname = '/quizzes/';
+            return;
+        }
+
+        await worker.start();
+    }
+
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
+}
+
+main();
