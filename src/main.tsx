127819510a84437c634a429a56370aa18bb39e80
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 17:07:25 +0200

    added readme.md

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import App from './App.tsx';
 import {USE_MOCK_API} from './config/env.ts';
 import {worker} from './mocks/browser.ts';
 
-if (USE_MOCK_API) worker.start();
+if (USE_MOCK_API)
+    worker.start({
+        waitUntilReady: true,
+        // msw is not working with vercel build, fix that
+        serviceWorker: {
+            url: '/mockServiceWorker.js',
+        },
+    });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
