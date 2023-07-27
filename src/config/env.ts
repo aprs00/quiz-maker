c5507a4f51799e7c8ab476031d433760f39185e4
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 17:25:18 +0200

    added readme.md

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+// export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = 'https://swapi.dev';
 export const USE_MOCK_API = import.meta.env.VITE_USE_MOCK_API;
