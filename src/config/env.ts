3e17f03016ae4719f63cd527a0929c807f260f9f
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-27 17:30:26 +0200

    added vercel.json

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,2 @@
-// export const API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = 'https://swapi.dev';
+export const API_URL = import.meta.env.VITE_API_URL;
 export const USE_MOCK_API = import.meta.env.VITE_USE_MOCK_API;
